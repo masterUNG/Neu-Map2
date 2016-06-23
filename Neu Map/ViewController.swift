04e364c8af220f4f1d4467916bb973abe87d9934
masterUNG <phrombutr@gmail.com>
2016-06-23 11:39:31 +0700

    Map OK

--- Neu Map/ViewController.swift
+++ Neu Map/ViewController.swift
@@ -7,13 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+       
+        var myLat:CLLocationDegrees = 16.422701
+        var myLng:CLLocationDegrees = 102.815245
+        
+        var latDelta:CLLocationDegrees = 0.01
+        var lngDelta:CLLocationDegrees = 0.01
+        
+        
+        var theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lngDelta)
+        var neuLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLng)
+        
+        var theRegion:MKCoordinateRegion = MKCoordinateRegionMake(neuLocation, theSpan)
+        
+        
+        self.myMapView.setRegion(theRegion, animated: true)
+        
+        
+        
+        
+    }   // viewDidLoad
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
